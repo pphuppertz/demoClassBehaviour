3a73a41c88c428eebd1a25ce47388d33800e3a8e
Peter Huppertz <phuppertz@linkassist.nl>
2019-09-11 13:53:12 +0200

    Specifically asked for female dev

--- demoClassBehaviour/Job.cs
+++ demoClassBehaviour/Job.cs
@@ -46,7 +46,7 @@ namespace demoClassBehaviour
 
             result.Add(new Job
             {
-                Name = "Junior C# Developer",
+                Name = "Female C# Developer",
                 Employer = "Dnl-Core",
                 Location = "Waarder",
                 PositionType = "Fully stuck developer",
